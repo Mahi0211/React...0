b6868523fe9a53267c7ab3e9c026700973a723b7
Mahi0211 <mahinpranave@gmail.com>
2025-05-23 15:20:26 +0530

    Changed Backend URL: Railway -> Render

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://react0-production.up.railway.app/api/menu/";
+export const MENU_API = "https://quickbite-811i.onrender.com/api/menu/";
 
 export const SWIGGY_API =
-  "https://react0-production.up.railway.app/api/restaurants";
+  "https://quickbite-811i.onrender.com/api/restaurants";
